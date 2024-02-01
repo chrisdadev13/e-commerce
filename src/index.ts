13c6b875470e9f212ae9f4a8ac0f3d569fec5121
Chris <capacheco.22@est.ucab.edu.ve>
2024-01-31 22:27:30 -0400

    feat: hono error handler

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import { serve } from "@hono/node-server";
+import { HTTPException } from "hono/http-exception";
 import { Hono } from "hono";
 import { connectDB } from "./config/db";
 
 import env from "./config/env";
 
 const port = env.PORT;
-
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+
+  return c.json({
+    code: 500,
+    message: "Something bad happened",
+  });
 });
 
-console.log(`Server is running on port ${port}`);
+console.log(`Server is running on port ${port} 🚀`);
 
 connectDB();
 
