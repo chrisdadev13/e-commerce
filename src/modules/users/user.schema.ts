c2f18d83221a64b6142bc5481fc83713fec09c20
Chris <capacheco.22@est.ucab.edu.ve>
2024-01-31 23:39:40 -0400

    fix: zod valid enum strings

--- src/modules/users/user.schema.ts
+++ src/modules/users/user.schema.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
-import { Role } from "./user.model";
+
+const Roles = ["Admin", "Customer"] as const;
 
 export const ZCreationSchema = z.object({
   name: z.string().min(1).max(50),
   email: z.string().email(),
   password: z.string().min(8).max(250),
-  role: z.nativeEnum(Role),
+  role: z.enum(Roles),
 });
 
 export type TCreationSchema = z.infer<typeof ZCreationSchema>;
