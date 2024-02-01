6160f599510470c7d8509dcaf475ff5bf839dbc0
Chris <capacheco.22@est.ucab.edu.ve>
2024-01-31 22:27:15 -0400

    feat: add user role (admin and client)

---
+++ src/modules/users/user.model.ts
@@ -0,0 +1,65 @@
+import { Document, Model, Schema, Types, model } from "mongoose";
+import { Bcrypt } from "oslo/password";
+
+export enum Role {
+  Admin,
+  Customer,
+}
+
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+  role: Role;
+  sessions: string[];
+}
+
+export type TUserModel = Model<User>;
+
+export type TUserDocument = Document<Types.ObjectId, any, User> & User;
+
+const userSchema = new Schema<User, TUserModel>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      enum: Object.values(Role),
+      default: Role.Customer,
+      required: true,
+    },
+    sessions: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const bcrypt = new Bcrypt();
+  const hashedPassword = await bcrypt.hash(this.password);
+
+  this.password = hashedPassword;
+
+  return next();
+});
+
+export const UserModel = model<User, TUserModel>("User", userSchema);
